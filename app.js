9c4aea602ecde862a1e77036e8aefada4e9dfb8d
craigsavage <craig.savage@ontariotechu.net>
2020-04-20 16:55:38 -0400

    added semantic ui

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const https = require('https');
 app.get('/', (req, res) => {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=812a0897dff7073fd0de02bdb7c87b01';
     https.get(url, (response) => {
-        console.log(response)
+        console.log('Status Code: ' + response.statusCode);
+
+        response.on('data', (data) => {
+            let weatherData = JSON.parse(data);
+
+            let temperature = weatherData.main.temp;
+            let feelsLike = weatherData.main.feels_like;
+
+            console.log(temperature);
+            console.log(feelsLike);
+        })
     })
 
     res.sendFile(__dirname + '/index.html');
