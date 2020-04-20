7739f63bb020874978e4ba2a9a9c9909660915e9
craigsavage <craig.savage@ontariotechu.net>
2020-04-20 17:49:05 -0400

    fixed layout

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const express = require('express'),
 const https = require('https');
 
 app.get('/', (req, res) => {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=812a0897dff7073fd0de02bdb7c87b01';
-    https.get(url, (response) => {
-        console.log('Status Code: ' + response.statusCode);
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=812a0897dff7073fd0de02bdb7c87b01';
+    // https.get(url, (response) => {
+    //     console.log('Status Code: ' + response.statusCode);
 
-        response.on('data', (data) => {
-            let weatherData = JSON.parse(data);
+    //     response.on('data', (data) => {
+    //         const weatherData = JSON.parse(data);
 
-            let temperature = weatherData.main.temp;
-            let feelsLike = weatherData.main.feels_like;
+    //         const city = weatherData.name;
+    //         const temperature = weatherData.main.temp;
+    //         const feelsLike = weatherData.main.feels_like;
+    //         const weatherDesc = weatherData.weather[0].description;
 
-            console.log(temperature);
-            console.log(feelsLike);
-        })
-    })
+    //         console.log(`City: ${city}.`);
+    //         console.log(`Temperature: ${temperature} degrees Celcius.`);
+    //         console.log(`Feels Like: ${feelsLike} degrees Celcius.`);
+    //         console.log(`${weatherDesc}`);
+    //     })
+    // })
 
     res.sendFile(__dirname + '/index.html');
 });
